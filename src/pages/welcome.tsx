a5e87c8717d31c15772c77e93b62649aec1fb0b7
Nick Felker <felker.nick@bk.ru>
2023-04-18 17:02:41 +0600

    1: init based welcome page

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -10,21 +10,25 @@ function WelcomePage(props:Props){
     return(
         <ExternalPage>
             <Header>
-                <LogoWrapper>Росреестр</LogoWrapper>
+                <LogoWrapper>
+                    <LogoText>
+                        Росреестр
+                    </LogoText>
+                </LogoWrapper>
                 <AuthorizationWrapper>
-                    <LoginLink>
-
-                    </LoginLink>
-                    <RegistrationLink>
-
-                    </RegistrationLink>
+                    <LoginLinkWrapper>
+                        <a href={"./login"}>Авторизация</a>
+                    </LoginLinkWrapper>
+                    <RegistrationLinkWrapper>
+                        <a href={"./signup"}>Регистрация</a>
+                    </RegistrationLinkWrapper>
                 </AuthorizationWrapper>
             </Header>
             <MainContent>
-
+                Хранилище кода
             </MainContent>
             <Footer>
-
+                Подвал сайта
             </Footer>
         </ExternalPage>
     )
@@ -34,28 +38,37 @@ const ExternalPage = styled.div`
     
 `
 const Header = styled.div`
-    
+    padding: 30px 50px 30px 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #DDDDDD;
 `
 const MainContent = styled.div`
-
+     padding: 50px 50px 50px 50px;
+     
 `
 const Footer = styled.div`
-
+    border-top: 1px solid #DDDDDD;
+    padding: 30px 50px 30px 50px;
 `
 const LogoWrapper = styled.div`
 
 `
 const LogoText = styled.h1`
-
+    font-size: 28px;
+    text-transform: uppercase;
 `
 
 const AuthorizationWrapper = styled.div`
-
+    display: flex;
+    align-items: center;
+    font-size: 18px;
 `
-const LoginLink = styled.a`
-
+const LoginLinkWrapper = styled.a`
+    margin-right: 20px;
 `
-const RegistrationLink = styled.a`
+const RegistrationLinkWrapper = styled.a`
 
 `
 
